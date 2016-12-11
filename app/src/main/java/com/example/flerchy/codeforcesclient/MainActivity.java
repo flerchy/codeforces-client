aae70363151cda37453ea31d9cfa54b5e62797ea
flerchy <flerchy@gmail.com>
2016-12-12 00:32:01 +0300

    init

---
+++ app/src/main/java/com/example/flerchy/codeforcesclient/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.flerchy.codeforcesclient;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    OkHttpClient client = new OkHttpClient();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url = "http://codeforces.com/api/recentActions?maxCount=5";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                    final String responseData = response.body().string();
+                    Log.d("res:", responseData);
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/example/flerchy/codeforcesclient/Post.java
@@ -0,0 +1,18 @@
+package com.example.flerchy.codeforcesclient;
+
+/**
+ * Created by flerchy on 12.12.2016.
+ */
+
+public class Post {
+
+    String title;
+    String author;
+    String content;
+
+    Post(String t, String a, String c) {
+        title = t;
+        author = a;
+        content = c;
+    }
+}
