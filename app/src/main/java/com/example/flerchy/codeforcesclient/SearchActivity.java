0c895227d066d99263d87151b808c888675ff240
flerchy <flerchy@gmail.com>
2016-12-13 20:12:15 +0300

    made asynctask instead of callback

--- app/src/main/java/com/example/flerchy/codeforcesclient/SearchActivity.java
+++ app/src/main/java/com/example/flerchy/codeforcesclient/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.example.flerchy.codeforcesclient;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
@@ -10,6 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.sql.Ref;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,10 +24,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class SearchActivity extends AppCompatActivity {
-    OkHttpClient client = new OkHttpClient();
-    public String userFirstName = new String();
-    public ResponseObject respobj = new ResponseObject();
-
+    RefreshFeedTask rfTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,38 +32,60 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     public void onSearchClick(View view) throws InterruptedException {
+        rfTask = new RefreshFeedTask();
         HttpUrl.Builder urlBuilder = HttpUrl.parse("http://codeforces.com/api/user.info").newBuilder();
         urlBuilder.addQueryParameter("handles", ((EditText) findViewById(R.id.user_name)).getText().toString());
-
         String url = urlBuilder.build().toString();
         Log.d("url:", url);
         Request request = new Request.Builder()
                 .url(url)
                 .build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onResponse(Call call, final Response response) throws IOException {
+
+        rfTask.execute(request);
+
+    }
+
+    class RefreshFeedTask extends AsyncTask<Request, Void, List<String>> {
+
+        private OkHttpClient client = new OkHttpClient();
+        private ResponseObject respobj = new ResponseObject();
+
+        @Override
+        protected List<String> doInBackground(Request... requests) {
+            List<String> userString = new ArrayList<>();
+            String userFirstName = new String();
+            try {
+                Response response = client.newCall(requests[0]).execute();
                 JSONParser parser = new JSONParser();
                 final String responseData = response.body().string();
                 Log.d("response:", responseData);
                 respobj = parser.parse(responseData);
                 Log.d("response status:", respobj.getStatus());
-                Result r = respobj.getResults()[0];
-                if (r != null) {
-                    userFirstName = r.getUser().getFirstName();
-                    Log.d("result:", userFirstName);
-                }
-            }
-
-            @Override
-            public void onFailure(Call call, IOException e) {
+            } catch (IOException e) {
                 Log.e("FAIL:", "FAIL");
+                e.printStackTrace();
             }
+            Log.d("response status:", respobj.getStatus());
+            Result r = respobj.getResults()[0];
+            if (r != null) {
+                userFirstName = r.getFirstName();
+                Log.d("result:", userFirstName);
+                Log.d("result:", r.getHandle());
+            }
+            userString.add(userFirstName);
+            Log.d("userString:", userString.get(0));
+            return userString;
+        }
 
-        });
-        TextView tvSearch = (TextView) findViewById(R.id.tvSearch);
-        tvSearch.setText(userFirstName);
-        Log.d("userFirstName=", userFirstName);
+        @Override
+        protected void onPostExecute(List<String> strings) {
+            super.onPostExecute(strings);
+            String userFirstName = strings.get(0);
+            Log.d("userString:", userFirstName);
+            TextView tvSearch = (TextView) findViewById(R.id.tvSearch);
+            tvSearch.setText(userFirstName);
+            Log.d("userFirstName=", userFirstName);
+        }
     }
 }
 
